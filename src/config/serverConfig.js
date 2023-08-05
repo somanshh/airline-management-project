28d6be2c1f6225402f0ca317be03fc607c10bb25
Somansh <rahulsoni0530@gmail.com>
2023-08-06 01:08:50 +0530

    added serverConfig.js file for handling .env

---
+++ src/config/serverConfig.js
@@ -0,0 +1,8 @@
+const envFile = require("dotenv")
+
+envFile.config();
+
+//console.log( process.env )
+
+module.exports =
+    process.env ;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log("created this file using cmd terminal")
+
+const express = require('express')
+//const PORT = require('./config/serverConfig.js');
+const obj = require('./config/serverConfig.js');
+
+const PORT = obj.PORT;
+const NAME = obj.NAME;
+
+// we have to require env package in order to use it
+//require('dotenv').config()
+
+const createAndStartServer = async () => {
+
+    // create a exp obj
+    const server = express()
+    
+    server.listen( PORT , () => {
+	console.log(`server started runnin at ${PORT}`)
+	console.log( `seprate file made for .env package by ${NAME}`)
+	} )
+	
+}
+
+createAndStartServer();
