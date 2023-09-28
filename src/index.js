44e709a686a429535d1e0a0239d4d9d90c2a4a98
Somansh <rahulsoni0530@gmail.com>
2023-09-28 12:53:48 +0530

    moved the config folder outside

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-console.log("created this file using cmd terminal");
+console.log("created this file using cmd terminal")
 
-const express = require("express");
-//const PORT = require('./config/serverConfig.js');
-const envObj = require("./config/serverConfig.js");
-const CityRepository = require("./repository/city-repository.js");
+const express = require('express')
+const bodyParser = require('body-parser')
+
+//const PORT = require('./config/serverConfig.js');	// one way to handle
+const envObj = require('../config/serverConfig.js'); 
+const CityRepository = require('./repository/city-repository')
 
 const PORT = envObj.PORT;
 const NAME = envObj.NAME;
 
-// we have to require env package in order to use it
-//require('dotenv').config()
-
 const createAndStartServer = async () => {
-  // create a exp obj
-  const server = express();
 
-  server.listen(PORT, () => {
-    console.log(`server started runnin at ${PORT}`);
-    console.log(`seprate file made for .env package by ${NAME}`);
-    // for creating city in db
-    const repo = new CityRepository();
-    console.log(CityRepository);
-    repo.createCity({ name: "delhi" });
-    // repo.createCity({ name: "delhi" });
-    repo.createCity({ name: "gurgaon" });
-  });
-};
+    // create a exp obj
+    const server = express()
+
+	// these are the middlewares , which will be called with each API call
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({extended: true}));
+    
+    server.listen( PORT , () => {
+	console.log(`server started runnin at ${PORT}`)
+		console.log( `seprate file made for .env package by ${NAME}`)
+	// for creating city in db
+	//const repo = new CityRepository();
+	//repo.createCity({name: "New Delhi"});
+	} )
+	
+}
 
 createAndStartServer();
